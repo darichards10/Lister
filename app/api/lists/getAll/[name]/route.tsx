d88ad38d95a5eac5a766114070fc3d760e71a81d
Drew Richards <98420057+darichards10@users.noreply.github.com>
2024-05-30 14:20:28 -0400

    updated get all route

--- app/api/lists/getAll/[name]/route.tsx
+++ app/api/lists/getAll/[name]/route.tsx
@@ -1,12 +1,12 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const { slug } = req.query; 
-   
-    return res.status(200).json({}); // Respond with an empty JSON object
-  } catch (error) {
-    console.error('Error fetching lists:', error);
-    return res.status(500).json({ error: 'Internal Server Error' }); // Respond with an error message
-  }
+import type { NextApiRequest, NextApiResponse } from 'next'
+ 
+type ResponseData = {
+  message: string
+}
+ 
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  res.status(200).json({ message: 'Hello from Next.js!' })
 }
--- app/api/lists/getAll/route.tsx
+++ app/api/lists/getAll/route.tsx
@@ -1,11 +1,9 @@
 import { getAllLists } from '../../../database/lib/database';
 import { NextResponse } from 'next/server';
 
-
 export async function GET() {
   try {
     const lists = await getAllLists(); 
-    console.log(lists)
     return NextResponse.json(lists, { status: 200 }); 
   } catch (error) {
     console.error('Error fetching lists:', error);
